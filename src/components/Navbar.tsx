6c500df1d17c9171f0fab256338a8e2a4976283d
Coelho <fvcoelho@gmail.com>
2025-07-31 12:08:34 -0300

    ajuste no logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,15 +10,15 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-lg fixed w-full z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+        <div className="flex justify-between h-20">
           <div className="flex items-center">
             <Link href="/" className="flex-shrink-0 flex items-center">
               <Image 
                 src="/logo.svg" 
                 alt="Pisos Pró - Venda & Instalação" 
-                width={150} 
-                height={60}
-                className="h-12 w-auto"
+                width={400} 
+                height={200}
+                className="h-16 w-auto"
                 priority
               />
             </Link>
